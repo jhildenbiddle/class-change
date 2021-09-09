9dde525a20857b78d70d70a3401debd84eb3488f
John Hildenbiddle <jhildenbiddle@users.noreply.github.com>
2021-09-09 13:09:45 -0500

    Add lint task

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,51 +1,53 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "mocha": true,
-        "node": true
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es6': true,
+        'mocha': true,
+        'node': true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:mocha/recommended"
+    'extends': [
+        'eslint:recommended',
+        'plugin:mocha/recommended'
     ],
-    "parserOptions": {
-        "sourceType": "module"
+    'ignorePatterns': [
+        'dist'
+    ],
+    'parserOptions': {
+        'sourceType': 'module'
     },
-    "plugins": [
-        "chai-expect",
-        "html",
-        "mocha"
+    'plugins': [
+        'chai-expect',
+        'html',
+        'mocha'
     ],
-    "rules": {
-        "array-bracket-spacing"      : ["error", "never"],
-        "array-callback-return"      : ["error"],
-        "block-scoped-var"           : ["error"],
-        "block-spacing"              : ["error", "always"],
-        "curly"                      : ["error"],
-        "dot-notation"               : ["error"],
-        "eqeqeq"                     : ["error"],
-        "indent"                     : ["error", 4],
-        "linebreak-style"            : ["error", "unix"],
-        "no-console"                 : ["warn"],
-        "no-floating-decimal"        : ["error"],
-        "no-implicit-coercion"       : ["error"],
-        "no-implicit-globals"        : ["error"],
-        "no-loop-func"               : ["error"],
-        "no-return-assign"           : ["error"],
-        "no-template-curly-in-string": ["error"],
-        "no-unneeded-ternary"        : ["error"],
-        "no-unused-vars"             : ["error", { "args": "none" }],
-        "no-useless-computed-key"    : ["error"],
-        "no-useless-return"          : ["error"],
-        "no-var"                     : ["error"],
-        "prefer-const"               : ["error"],
-        "quotes"                     : ["error", "single"],
-        "semi"                       : ["error", "always"],
+    'rules': {
+        'array-bracket-spacing'      : ['error', 'never'],
+        'array-callback-return'      : ['error'],
+        'block-scoped-var'           : ['error'],
+        'block-spacing'              : ['error', 'always'],
+        'curly'                      : ['error'],
+        'dot-notation'               : ['error'],
+        'eqeqeq'                     : ['error'],
+        'indent'                     : ['error', 4],
+        'no-console'                 : ['warn'],
+        'no-floating-decimal'        : ['error'],
+        'no-implicit-coercion'       : ['error'],
+        'no-implicit-globals'        : ['error'],
+        'no-loop-func'               : ['error'],
+        'no-return-assign'           : ['error'],
+        'no-template-curly-in-string': ['error'],
+        'no-unneeded-ternary'        : ['error'],
+        'no-unused-vars'             : ['error', { 'args': 'none' }],
+        'no-useless-computed-key'    : ['error'],
+        'no-useless-return'          : ['error'],
+        'no-var'                     : ['error'],
+        'prefer-const'               : ['error'],
+        'quotes'                     : ['error', 'single'],
+        'semi'                       : ['error', 'always'],
 
-        "mocha/no-hooks-for-single-case": ["off"],
-        "mocha/no-top-level-hooks"      : ["off"],
-        "mocha/no-setup-in-describe"    : ["off"]
+        'mocha/no-hooks-for-single-case': ['off'],
+        'mocha/no-top-level-hooks'      : ['off'],
+        'mocha/no-setup-in-describe'    : ['off']
     }
 };
